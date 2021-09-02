2f2139faf392d789e46ba70e66c6a3cefb203045
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-02 09:12:17 -0300

    Updates

---
+++ precache-manifest.53d4f2222659f543bbda9bb3a20cae2c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aee92ccdef235bde95a21aa91b74553",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39676c38fc7535d1deb3",
+    "url": "./static/css/2.a7f183f2.chunk.css"
+  },
+  {
+    "revision": "f4e655cce35f8f76cd7b",
+    "url": "./static/css/main.1c95b790.chunk.css"
+  },
+  {
+    "revision": "39676c38fc7535d1deb3",
+    "url": "./static/js/2.84721c38.chunk.js"
+  },
+  {
+    "revision": "ebee8e53c5fb95b35c9b9f020399c85e",
+    "url": "./static/js/2.84721c38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4e655cce35f8f76cd7b",
+    "url": "./static/js/main.0e3da1c0.chunk.js"
+  },
+  {
+    "revision": "ec6ae438e9e64eba8102",
+    "url": "./static/js/runtime-main.3103008a.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "./static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "./static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7861302ee59fe9c809ca615ae8681e36",
+    "url": "./static/media/erro404.7861302e.jpg"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "./static/media/trivia.466d153e.png"
+  }
+]);
